78f98224f8daff766be75dfdc9ebda2362abca24
Maaz Hasan <maazhassan2@gmail.com>
2019-07-14 14:24:47 +0500

    feat: Create ML pipeline

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,83 @@
 import sys
+import nltk
+import pickle
+import pandas as pd
+
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import GridSearchCV
+from sqlalchemy import create_engine
+from sklearn.multioutput import MultiOutputClassifier
 
 
 def load_data(database_filepath):
-    pass
 
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    table_name = 'disaster_relief'
+    df = pd.read_sql_table(table_name, engine)
+    category_names = df.columns.tolist()
+
+
+    # load data from database
+
+    df = df[df['genre'] == 'direct']
+    X = df.message.values
+    y = df.drop(columns=['id', 'message', 'original', 'genre'])
+
+
+    return X, y, category_names
 
 def tokenize(text):
-    pass
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [20, 50, 100]
+    }
+    model = GridSearchCV(pipeline, param_grid=parameters)
+    return model
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+
+    y_pred = model.predict(X_test)
+
+    for i in range(len(category_names)):
+        print("Category:", category_names[i], "\n", classification_report(Y_test.iloc[:, i].values, y_pred[:, i]))
+        print('Accuracy of %25s: %.2f' % (category_names[i], accuracy_score(Y_test.iloc[:, i].values, y_pred[:, i])))
+        print('---------------------------------------------------------------------------')
+    print("\nBest Parameters:", model.best_params_)
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():

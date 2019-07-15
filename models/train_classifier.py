159a0acc23ad6e050a81b64fda3b700e11a2c5f8
Maaz Hasan <maazhassan2@gmail.com>
2019-07-15 12:24:01 +0500

    refactor: ML pipeline train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,48 +1,59 @@
-import sys
-import nltk
-import pickle
-import pandas as pd
-
-nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
-
-
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import GridSearchCV
 from sqlalchemy import create_engine
 from sklearn.multioutput import MultiOutputClassifier
+import sys
+import nltk
+import pickle
+import pandas as pd
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 
 def load_data(database_filepath):
+    """Load data into dataframe from database.
+
+    Args:
+    database_filepath: str. The path of the database file.
+
+    Returns:
+    X: feature data values
+    y: class variables
+    category_names: list of feature coloumns
+    """
 
     # load data from database
     engine = create_engine('sqlite:///{}'.format(database_filepath))
     table_name = 'disaster_relief'
     df = pd.read_sql_table(table_name, engine)
     category_names = df.columns.tolist()
-
-
-    # load data from database
-
+    # select only messeges recieved directly for greater accuracy
     df = df[df['genre'] == 'direct']
     X = df.message.values
     y = df.drop(columns=['id', 'message', 'original', 'genre'])
-
-
     return X, y, category_names
 
+
 def tokenize(text):
+    """Clean and tokenize data recieved.
+
+    Args:
+    text: str. Corpus of raw data.
+
+    Returns:
+    clean_tokens: list of tokens
+    """
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
     clean_tokens = []
+    # Clean tokens and add to list
     for tok in tokens:
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
@@ -50,6 +61,12 @@ def tokenize(text):
 
 
 def build_model():
+    """Create machine learning model using count vectorizer and tf-idf transfomer. Use Gridseach to
+    find the best parameters.
+
+    Returns:
+    model: GridSearchCV
+    """
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
@@ -61,19 +78,27 @@ def build_model():
         'tfidf__use_idf': (True, False),
         'clf__estimator__n_estimators': [20, 50, 100]
     }
-    model = GridSearchCV(pipeline, param_grid=parameters)
-    return model
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
+    """Clean and tokenize data recieved.
 
+    Args:
+    text: str. Corpus of raw data.
+
+    Returns:s
+    clean_tokens: list of tokens
+    """
     y_pred = model.predict(X_test)
 
     for i in range(len(category_names)):
-        print("Category:", category_names[i], "\n", classification_report(Y_test.iloc[:, i].values, y_pred[:, i]))
-        print('Accuracy of %25s: %.2f' % (category_names[i], accuracy_score(Y_test.iloc[:, i].values, y_pred[:, i])))
         print('---------------------------------------------------------------------------')
-    print("\nBest Parameters:", model.best_params_)
+        print("Category:", category_names[i])
+        print(classification_report(Y_test.iloc[:, i].values, y_pred[:, i]))
+        print("Accuracy: ", accuracy_score(Y_test.iloc[:, i].values, y_pred[:, i]))
+    print("Best Parameters:", model.best_params_)
 
 
 def save_model(model, model_filepath):
@@ -86,13 +111,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
@@ -102,9 +127,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
